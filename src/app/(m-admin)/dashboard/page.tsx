8849818bce16e850d7a340e5290f5162e1325188
Color Hut Official <colorhut.official@gmail.com>
2025-06-16 05:47:44 +0000

    hide all sidebar for /m-admin login page

--- src/app/(m-admin)/dashboard/page.tsx
+++
@@ -1,3 +0,0 @@
-// This file is part of the old (m-admin) route group which is no longer in use.
-// It can be considered for deletion.
-// Kept as a placeholder to avoid routing conflicts due to potential caching.
--- src/app/(m-admin)/page.tsx
+++
@@ -1,4 +0,0 @@
-// This route group (m-admin) is no longer in use and can be considered for deletion.
-// It has been replaced by functionality under /src/app/(app)/m-admin/
-// Keeping this file as a placeholder to prevent Next.js from attempting to route it
-// if any caching issues persist.
