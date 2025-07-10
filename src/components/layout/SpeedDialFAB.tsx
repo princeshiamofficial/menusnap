67af7227d99094d4dca2531a38c8328318fe2b74
Color Hut Official <colorhut.official@gmail.com>
2025-07-10 14:50:51 +0000

    i want to show it on top for mobile

--- src/components/layout/SpeedDialFAB.tsx
+++ src/components/layout/SpeedDialFAB.tsx
@@ -164,7 +164,7 @@ export function SpeedDialFAB(): ReactNode {
 
 
   return (
-    <div className="fixed bottom-4 right-4 flex flex-col items-end z-50">
+    <div className="fixed top-16 right-4 flex flex-col items-end z-50 md:top-auto md:bottom-4">
       <AnimatePresence>
         {showHelpTextVisual && !isOpen && (
           <motion.div
