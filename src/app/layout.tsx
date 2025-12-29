bfe4bfffd64dc979bc3bba57872445d8ce01a915
Color Hut Official <colorhut.official@gmail.com>
2025-12-29 08:31:37 +0000

    One pixel found on 6000-firebase-studio-1748612250399.cluster-nzwlpk54dv

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@
 
 import type { ReactNode } from 'react';
 import { Inter, Roboto_Mono } from 'next/font/google';
-import Script from 'next/script';
 import './globals.css';
 import { ClientSideOnlyToaster } from '@/components/layout/client-side-only-toaster';
 import { ThemeProvider } from '@/context/ThemeContext';
@@ -28,26 +27,11 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const pixelId = "455555256";
   return (
     <html lang="en" suppressHydrationWarning={true}>
       <head>
-        {/* Meta Pixel Code */}
-        <Script id="meta-pixel-script" strategy="afterInteractive">
-          {`
-            !function(f,b,e,v,n,t,s)
-            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-            n.queue=[];t=b.createElement(e);t.async=!0;
-            t.src=v;s=b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t,s)}(window, document,'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '${pixelId}');
-            fbq('track', 'PageView');
-          `}
-        </Script>
-        {/* End Meta Pixel Code */}
+        {/* The hardcoded Meta Pixel script has been removed from here to prevent duplication. */}
+        {/* The MetaPixelScriptLoader component below now handles script injection. */}
       </head>
       <body className={`${inter.variable} ${robotoMono.variable} font-sans antialiased`} suppressHydrationWarning={true}>
         <ClientAuthProvider>
