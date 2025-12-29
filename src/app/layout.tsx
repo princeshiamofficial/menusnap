f876879739e1fbfa7cd257f5127f4e576815348c
Color Hut Official <colorhut.official@gmail.com>
2025-12-29 08:21:04 +0000

    also use:        {/* Meta Pixel Code */}         <Script id="meta-pixel-

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,13 +26,28 @@ const robotoMono = Roboto_Mono({
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.Node;
+  children: React.ReactNode;
 }>) {
+  const pixelId = "455555256";
   return (
     <html lang="en" suppressHydrationWarning={true}>
       <head>
-        {/* The hardcoded Meta Pixel script has been removed from here. */}
-        {/* The new MetaPixelScriptLoader component now handles script injection. */}
+        {/* Meta Pixel Code */}
+        <Script id="meta-pixel-script" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '${pixelId}');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        {/* End Meta Pixel Code */}
       </head>
       <body className={`${inter.variable} ${robotoMono.variable} font-sans antialiased`} suppressHydrationWarning={true}>
         <ClientAuthProvider>
