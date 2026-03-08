fe546cff61473724dd48168f2e21514a93953146
Prince Shiam <prince.shiam.official@gmail.com>
2026-03-08 21:32:03 +0600

    chore: add pm2 ecosystem config for hosting

---
+++ ecosystem.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  apps: [
+    {
+      name: 'menusnap-app',
+      script: 'node_modules/next/dist/bin/next',
+      args: 'start -p 3000',
+      instances: 'max',
+      exec_mode: 'cluster',
+      env: {
+        NODE_ENV: 'production',
+      },
+    },
+    {
+      name: 'menusnap-collab',
+      script: 'collab-server.mjs',
+      instances: 1,
+      env: {
+        NODE_ENV: 'production',
+        COLLAB_PORT: 1234,
+      },
+    },
+  ],
+};
