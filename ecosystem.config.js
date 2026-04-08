986e792a890b7c56ded68a67fe19f2e51b91e3e1
Prince Shiam <prince.shiam.official@gmail.com>
2026-04-08 14:57:27 +0600

    Update ecosystem config and other modifications

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,6 +5,12 @@ module.exports = {
       script: 'server.js',
       instances: 1,
       exec_mode: 'fork',
+      watch: true,
+      watch_options: {
+        usePolling: true,
+        interval: 1000,
+        ignored: ['node_modules', '.git']
+      },
       env: {
         NODE_ENV: 'production',
         PORT: 3007,
