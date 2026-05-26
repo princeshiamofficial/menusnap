7e7039326d4866fe22504bb8a185f32cdd240504
developer <developer@menusnap.com>
2026-05-27 04:55:06 +0600

    feat: change navbar logo href link to /free-design

--- src/components/free-design/FreeDesignNavbar.tsx
+++ src/components/free-design/FreeDesignNavbar.tsx
@@ -104,7 +104,7 @@ export function FreeDesignNavbar() {
       >
         <div className="flex items-center justify-between">
           {/* Logo */}
-          <Link href="/dashboard/" className="flex items-center gap-2 group">
+          <Link href="/free-design" className="flex items-center gap-2 group">
             <div className="bg-black px-4 py-2 rounded-xl transition-transform group-hover:scale-105 flex items-center justify-center">
               <img 
                 src="/menusnap-logo-white.png" 
