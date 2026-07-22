710f70cc6273ab43809b8c55be09a62183be6eaa
developer <developer@menusnap.com>
2026-07-22 15:43:55 +0600

    fix(bottom-nav): realign center grid spacer width to 80px eliminating overlap with Drafts

--- src/components/layout/bottom-navigation.tsx
+++ src/components/layout/bottom-navigation.tsx
@@ -106,7 +106,7 @@ export function BottomNavigation() {
           })}
 
           {/* Center Notch Spacer */}
-          <div className="w-[24px] h-full" />
+          <div className="w-[80px] h-full" />
 
           {/* Right Nav Items */}
           {navItemsRight.map((item) => {
