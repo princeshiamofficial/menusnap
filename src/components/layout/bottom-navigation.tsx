6ee79343a1ecad9df08c5e42833602f75ed9967a
developer <developer@menusnap.com>
2026-07-22 15:28:31 +0600

    style(bottom-nav): set mobile bottom navigation height to exactly 80px

--- src/components/layout/bottom-navigation.tsx
+++ src/components/layout/bottom-navigation.tsx
@@ -40,7 +40,7 @@ export function BottomNavigation() {
 
   return (
     <div data-wf--new-navbar--variant="primary" className="rd-navbar_wrapper md:hidden">
-      <div className="relative w-full h-[88px] pointer-events-auto">
+      <div className="relative w-full h-[80px] pointer-events-auto">
         {/* Main Grid Navigation */}
         <nav className="rd-navbar w-full h-full">
           {/* Curve Shape SVG Background */}
